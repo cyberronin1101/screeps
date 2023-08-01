2f239d437e288678dc08025b23c2ffd87c4725c6
Yuriy Burilov <yuriy.burilov@umbrellait.com>
2023-08-02 02:53:11 +0400

    tutorial 02 (test loop)

--- src/loop.ts
+++ src/loop.ts
@@ -5,6 +5,8 @@ module.exports.loop = () => {
   for (const name in Game.creeps) {
     const creep = Game.creeps[name];
 
+    console.log("creep", creep);
+
     switch (creep.memory.role) {
       case "harvester":
         roleHarvester.run(creep);
