0698e50e2f2fab038e5516a13fcbb127406e1e9d
Yuriy Burilov <yuriy.burilov@umbrellait.com>
2023-08-02 02:48:44 +0400

    tutorial 02

--- src/loop.ts
+++ src/loop.ts
@@ -1,7 +1,17 @@
 import { roleHarvester } from "./roles/harvester.role";
+import { roleUpgrader } from "./roles/upgrader.role";
 
 module.exports.loop = () => {
   for (const name in Game.creeps) {
-    roleHarvester.run(Game.creeps[name]);
+    const creep = Game.creeps[name];
+
+    switch (creep.memory.role) {
+      case "harvester":
+        roleHarvester.run(creep);
+        return;
+      case "upgrader":
+        roleUpgrader.run(creep);
+        return;
+    }
   }
 };
